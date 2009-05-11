7d0ae8eacbd84ec3b57be6d416e6ec1ad98d0ded
Wlodek Bzyl <matwb@univ.gda.pl>
2009-05-11 22:24:13 +0200

    use Rack::Test instead of Sinatra::Test

--- test/sinatra_rdiscount_test.rb
+++ test/sinatra_rdiscount_test.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class SinatraRDiscountTest < Test::Unit::TestCase
-  include Sinatra::Test
+  include Rack::Test::Methods
   
   def rdiscount_app(&block)
     mock_app {
       set :views, File.dirname(__FILE__) + '/views'      
-      helpers Sinatra::RDiscountTemplate
+      helpers Sinatra::RDiscount
       set :show_exceptions, false
       get '/', &block
     }
@@ -15,20 +15,20 @@ class SinatraRDiscountTest < Test::Unit::TestCase
   
   def test_renders_inline_strings
     rdiscount_app { rdiscount 'hello world' }
-    assert ok?
-    assert_equal "<p>hello world</p>\n", body
+    assert last_response.ok?
+    assert_equal "<p>hello world</p>\n", last_response.body
   end
   
   def test_renders_inline_erb_string
     rdiscount_app { rdiscount '{%= 1 + 1 %}' }
-    assert ok?
-    assert_equal "<p>2</p>\n", body
+    assert last_response.ok?
+    assert_equal "<p>2</p>\n", last_response.body
   end
 
   def test_renders_files_in_views_path
     rdiscount_app { rdiscount :hello }
-    assert ok?
-    assert_equal "<h1>hello world</h1>\n", body
+    assert last_response.ok?
+    assert_equal "<h1>hello world</h1>\n", last_response.body
   end
   
   def test_takes_locals_option
@@ -36,30 +36,30 @@ class SinatraRDiscountTest < Test::Unit::TestCase
       locals = {:foo => 'Bar'}
       rdiscount "{%= foo %}", :locals => locals
     }
-    assert ok?
-    assert_equal "<p>Bar</p>\n", body
+    assert last_response.ok?
+    assert_equal "<p>Bar</p>\n", last_response.body
   end
 
   def test_renders_with_inline_layouts
     rdiscount_app {
       rdiscount 'Sparta', :layout => 'THIS. IS. <%= yield.upcase %>' 
     }
-    assert ok?
-    assert_equal "THIS. IS. <P>SPARTA</P>\n", body
+    assert last_response.ok?
+    assert_equal "THIS. IS. <P>SPARTA</P>\n", last_response.body
   end
 
   def test_renders_with_file_layouts
     rdiscount_app {
       rdiscount 'hello world', :layout => :layout2
     }
-    assert ok?
-    assert_equal "erb layout\n<p>hello world</p>\n\n", body
+    assert last_response.ok?
+    assert_equal "erb layout\n<p>hello world</p>\n\n", last_response.body
   end
 
   def test_renders_erb_with_blocks
     mock_app {
       set :views, File.dirname(__FILE__) + '/views'      
-      helpers Sinatra::RDiscountTemplate
+      helpers Sinatra::RDiscount
       
       def container
         yield
@@ -74,14 +74,14 @@ class SinatraRDiscountTest < Test::Unit::TestCase
     }
     
     get '/'
-    assert ok?
-    assert_equal "<p> THIS. IS. SPARTA! </p>\n", body
+    assert last_response.ok?
+    assert_equal "<p> THIS. IS. SPARTA! </p>\n", last_response.body
   end
   
   def test_raises_error_if_template_not_found
     mock_app {
       set :views, File.dirname(__FILE__) + '/views'      
-      helpers Sinatra::RDiscountTemplate
+      helpers Sinatra::RDiscount
       set :show_exceptions, false
       
       get('/') { rdiscount :no_such_template }
